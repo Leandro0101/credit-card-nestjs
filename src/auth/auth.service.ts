01a973748fc8c8b51161e509fdbe99baceb99852
leandro3306 <leandrovieira3306@gmail.com>
2022-05-01 20:09:30 -0300

    feat: compare passwords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(
@@ -11,10 +11,15 @@ export class AuthService {
 
   async login(email: string, password: string) {
     const user = await this.userService.getUserByEmail(email);
-    if (!user) throw new UnauthorizedException();
-    const payload = { email, sub: user.id };
-    return {
-      token: this.jwtService.sign(payload),
-    };
+    if (user) {
+      const isValid = await bcrypt.compare(password, user.password);
+      if (isValid) {
+        const payload = { email, sub: user.id };
+        return {
+          token: this.jwtService.sign(payload),
+        };
+      }
+    }
+    throw new UnauthorizedException();
   }
 }
